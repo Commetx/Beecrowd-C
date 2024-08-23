29f651990eb8b315b562f394d7ff8883561e1f4c
Commetx <11.gabrielsm@gmail.com>
2024-08-22 21:38:02 -0300

    Add files via upload

---
+++ Iniciante/1000.c
@@ -0,0 +1,11 @@
+// 1000 Beecrowd (Hello World!)
+// Por Commetx
+#include <stdio.h>
+
+int main() {
+
+    // Exibindo a mensagem
+    printf("Hello World!\n");
+
+    return 0;
+}
---
+++ Iniciante/1011.c
@@ -0,0 +1,25 @@
+// 1011 Beecrowd (Esfera)
+// Por Commetx
+#include <stdio.h>
+// Incluindo a biblioteca matemática 
+#include <math.h>
+
+int main() {
+
+    // Declarando variável de valor inteiro (raio)
+    int r;
+    // Declarando variáveis de valores decimais
+    double pi, volume;
+    // Lendo os valores de cada variável
+    scanf("%d", &r);
+
+    // Afirmando o valor de pi
+    pi = 3.14159;
+    // Calculando o valor do volume da esfera
+    volume = 4.0/3.0 * pi * pow(r, 3);
+
+    // Imprimindo o valor do volume da esfera conforme requisitado
+    printf("VOLUME = %.3lf\n", volume);
+    
+    return 0;
+}
